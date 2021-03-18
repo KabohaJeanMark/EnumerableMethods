57a7e54cddb2ba8dff99fc8beb07787fc9e2a71e
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-03-18 12:24:58 +0300

    Add my_each enum method

--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
-def initial_method
-  # initialize method
+module Enumerable
+  def my_each
+    i = 0
+    until i == size
+      yield(self[i])
+      i += 1
+    end
+  end
 end
+
+# example to show my_each is working
+[1, 2, 3].my_each { |num| puts num * 2 }
