e636c506ddab419e3aa1685987204cf284c240b0
Shaher-11 <shahershamroukh@gmail.com>
2021-03-18 11:32:16 +0200

    Add my_each_with_index method

--- main.rb
+++ main.rb
@@ -6,7 +6,18 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_each_with_index
+    i = 0
+    until i == size
+      yield(self[i], i)
+      i += 1
+    end
+  end
 end
 
 # example to show my_each is working
 [1, 2, 3].my_each { |num| puts num * 2 }
+
+# example to show my_each_with_index is working
+[1, 2, 3].my_each_with_index { |n, i| puts "the item is #{n} and the index is #{i}" }
