972f65e9013efe111d5fc48bde0235fcd1952610
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-03-18 13:46:56 +0300

    Add my_select enum method

--- main.rb
+++ main.rb
@@ -14,6 +14,12 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_select
+    selected_arr = []
+    to_a.my_each { |item| selected_arr.push(item) if yield item }
+    selected_arr
+  end
 end
 
 # example to show my_each is working
@@ -21,3 +27,6 @@ end
 
 # example to show my_each_with_index is working
 [1, 2, 3].my_each_with_index { |n, i| puts "the item is #{n} and the index is #{i}" }
+
+example_select = %w[Shaher jean mark Jordy Stranger].my_select { |name| name != 'Stranger' }
+puts example_select
